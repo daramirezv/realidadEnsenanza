055a2638daa95babebae524540d24bd92bb04cba
david andres ramirez <da.ramirezv@uniandes.edu.co>
2019-04-02 07:23:28 -0500

    primer commit

---
+++ Mate/Assets/Codigo/codigoTodo.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class codigoTodo : MonoBehaviour
+{
+    public AudioClip music1;
+    public AudioClip music2;
+    public AudioClip music3;
+    public AudioSource musicSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        musicSource.clip = music1;
+        musicSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!musicSource.isPlaying)
+        {
+            if (SteamVR_Input._default.inActions.GrabPinch.GetStateUp(SteamVR_Input_Sources.LeftHand))
+            {
+                musicSource.clip = music2;
+                musicSource.Play();
+            }
+
+            else if (SteamVR_Input._default.inActions.GrabPinch.GetStateUp(SteamVR_Input_Sources.RightHand))
+            {
+                musicSource.clip = music3;
+                musicSource.Play();
+            }
+        }
+    }
+}
